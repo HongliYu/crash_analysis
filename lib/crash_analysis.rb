2accb81c19ea093b7442adce1490c27333307688
HongliYu <yhlssdone@gmail.com>
2015-11-20 15:28:45 +0800

    version 0.1.3

--- lib/crash_analysis.rb
+++ lib/crash_analysis.rb
@@ -1,45 +1,6 @@
 require "crash_analysis/version"
 
-def traverse(filePath)
-  crashFileNames = Array.new
-  countApp = 0
-  countDSYM = 0
-  if File.directory?(filePath)
-    Dir.foreach(filePath) do |fileName|
-      fileSuffixArray = fileName.strip.split(".")
-      if fileSuffixArray.last == "crash"
-        fileSuffixArray.pop
-        crashFileNames << (filePath + "/" + fileSuffixArray.first)
-      end
-      if fileSuffixArray.last == "app"
-        countApp += 1
-      end
-      if fileSuffixArray.last == "dSYM"
-        countDSYM += 1
-      end
-    end
-  else
-    puts "Files:" + filePath
-  end
-
-  if countApp != 1 || countDSYM !=1
-      puts "error:\n"
-      puts "make sure the directory contains those files: one .app file & one .dSYM file & related crash files"
-    return
-  end
-
-  return crashFileNames
-end
-
-def AnalysisLog(crashFileNames)
-  cmd = "/Applications/Xcode.app/Contents/SharedFrameworks/DTDeviceKitBase.framework/Versions/A/Resources/symbolicatecrash"
-  evn = "export DEVELOPER_DIR='/Applications/XCode.app/Contents/Developer'"
-  crashFileNames.prog_each {
-   |fileName|
-    system("#{evn} \n #{cmd} #{fileName}.crash BDPhoneBrowser.app.dSYM > #{fileName}.log")
-  }
-end
-
+# Utils
 class Array
   def prog_each(&block) 
     bar_length = (`tput cols` || 80).to_i - 30
@@ -69,22 +30,92 @@ class Array
   end
 end
 
+# Main
 module CrashAnalysis
-  def self.run(filePath)
+  Version = "0.1.3"
+  def self.run(filePath, rawFileSuffix)
     puts "running..."
-    if filePath.nil? || filePath.empty?
-      puts "error: need directory path"
-    else
-      if File.directory?(filePath)
-        crashFileNames = traverse(filePath)
-        if crashFileNames.nil? || crashFileNames.empty?
+    analysis = Analysis.new()
+    analysis.run(filePath, rawFileSuffix)
+  end
+
+  class Analysis
+
+    def initialize()
+      @percentCount = 0
+    end
+
+    def run(filePath, rawFileSuffix)
+      if rawFileSuffix.nil? || rawFileSuffix.empty?
+          puts "error: need 2 arguments DirPath & raw log file suffix like:txt, log, crash..."
           return
-        else
-           AnalysisLog(crashFileNames)
         end
-      else
-        puts "error: not a directory"
+        if filePath.nil? || filePath.empty?
+          puts "error: need directory path"
+        else
+          if File.directory?(filePath)
+            crashFileNames = traverse(filePath, rawFileSuffix)
+            if crashFileNames.nil? || crashFileNames.empty?
+              return
+            else
+               AnalysisLog(crashFileNames, rawFileSuffix, filePath)
+            end
+          else
+            puts "error: not a directory"
+          end
+        end
+    end
+
+    def traverse(filePath, rawFileSuffix)
+    crashFileNames = Array.new
+    countApp = 0
+    countDSYM = 0
+    if File.directory?(filePath)
+      Dir.foreach(filePath) do |fileName|
+        fileSuffixArray = fileName.strip.split(".")
+        if fileSuffixArray.last == rawFileSuffix
+          fileSuffixArray.pop
+          crashFileNames << (filePath + "/" + fileSuffixArray.first)
+        end
+        if fileSuffixArray.last == "app"
+          countApp += 1
+        end
+        if fileSuffixArray.last == "dSYM"
+          countDSYM += 1
+        end
       end
+    else
+      puts "Files:" + filePath
+    end
+
+    if countApp != 1 || countDSYM !=1 || crashFileNames.count < 1
+        puts "error:\n"
+        puts "make sure the directory contains those files: one .app file & one .dSYM file & related crash files"
+      return
+    end
+
+    return crashFileNames
+  end
+
+    def AnalysisLog(crashFileNames, rawFileSuffix, filePath)
+      cmd = "/Applications/Xcode.app/Contents/SharedFrameworks/DTDeviceKitBase.framework/Versions/A/Resources/symbolicatecrash"
+      evn = "export DEVELOPER_DIR='/Applications/XCode.app/Contents/Developer'"
+      logDir = filePath + "/crash_logs"
+
+      if !File.directory?(logDir)
+        Dir.mkdir(logDir)
+      end
+
+      for fileName in crashFileNames
+        shortFileName = fileName.split("/").last
+        outputFile = logDir + "/" + shortFileName +".log"
+        system("#{evn} \n #{cmd} #{fileName}.#{rawFileSuffix} BDPhoneBrowser.app.dSYM > #{outputFile}")
+        @percentCount = @percentCount + 1
+        precent = ((@percentCount.to_f / crashFileNames.count.to_f) * 10000).round / 10000.0
+        str = (precent * 100).to_s
+        print "\r #{str[0,4]}%"
+      end
+      puts "\n Done."
     end
   end
 end
